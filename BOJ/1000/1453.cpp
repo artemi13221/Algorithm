ceca82441149e6a8eb115efb91fb6a6e5dbcdcd6
root <root@DESKTOP.localdomain>
2020-11-21 14:25:22 +0900

    BOJ

---
+++ BOJ/1000/1453.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int arr[101] = {0, };
+    int num;
+    int cnt = 0;
+    cin >> num;
+
+    for(int i = 0; i < num; i++) {
+        int tmp;
+        cin >> tmp;
+        if(arr[tmp] == 1) {
+            cnt++;
+        }else {
+            arr[tmp] = 1;
+        }
+    }
+
+    cout << cnt;
+}
