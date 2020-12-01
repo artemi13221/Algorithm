427bf93867f9557083da3427885596234e15aad0
artemi <artemi13221@gmail.com>
2020-12-02 04:32:44 +0900

    [BOJ]

---
+++ BOJ/9000/9656.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int num = 0;
+    cin >> num;
+
+    if(num % 2 == 0) {
+        cout << "SK";
+    }
+    else {
+        cout << "CY";
+    }
+}
